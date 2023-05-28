2290c7205682c1254d7cfd9e61ce8de3e48e23f9
Gershon Bialer <gershon.bialer@gmail.com>
2023-05-28 10:42:58 -0700

    Initial commit

---
+++ api.py
@@ -0,0 +1,83 @@
+import olca_ipc as ipc
+import olca_schema as o
+from flask import Flask
+import json
+import openai
+import chromadb
+from chromadb.utils import embedding_functions
+from flask import request
+import os
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from chromadb.config import Settings
+
+openai.api_key= os.environ['OPENAI_API_KEY']
+
+app = Flask(__name__)
+
+
+chroma_client = chromadb.Client(Settings(chroma_db_impl='duckdb+parquet',persist_directory='db'))
+
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=os.environ['OPENAI_API_KEY'],
+                model_name="text-embedding-ada-002"
+            )
+collection = chroma_client.get_collection(name="flow_collection",embedding_function=openai_ef)
+    
+
+PROMPT = """You are an LCA analyst.
+Extract a list of flows. When data is not available, use your best guess to give exact numbers. Return the answer in JSON-array with the fields:
+- flowName: Name of flow
+- process: Description of how flow
+- location
+- Weight: in kg
+Product Info:
+```
+<DESCRIPTION>
+```
+"""
+def get_flows(description):
+    messages = [{'role':'user', 'content':PROMPT.replace('<DESCRIPTION>',description)}]
+    response = openai.ChatCompletion.create(
+        model='gpt-4',
+        messages=messages,
+        temperature=0, 
+        max_tokens=2000)
+    print(response)
+    return json.loads(response['choices'][0]['message']['content'])
+
+
+@app.route("/impact")
+def get_impact():
+    product_desc = request.args.get('product_desc')
+    flows = get_flows(product_desc)
+    q = collection.query(query_texts=[flows[0]['flowName']])
+    client = ipc.Client(port=8083)
+    n = 0
+    while n < len(q['ids'][0]):
+        oid = q['ids'][0][n]
+        f = client.get(o.Flow, oid)
+        providers = client.get_providers(o.Ref(id=f.id))
+        if len(providers) > 0:
+            break
+        n += 1
+    process_ref = providers[0].provider
+    config = o.LinkingConfig(
+        prefer_unit_processes=True,
+        provider_linking=o.ProviderLinking.PREFER_DEFAULTS,
+    )
+    system_ref = client.create_product_system(process_ref, config)
+
+    setup = o.CalculationSetup(
+        target=system_ref,
+        impact_method=impact_methods[0],
+    )
+    result = client.calculate(setup)
+    result.wait_until_ready()
+    impacts = result.get_total_impacts()
+    impact_json = []
+    for i in impacts:
+        impact_json.append({'impact_category_name':i.impact_category.name,'amount':i.amount, 'unit':ri.impact_category.ref_unitef_unit})
+    return json_response(impact_json)
+
+
+app.run(host='0.0.0.0', port=5001)
---
+++ loadFlows.py
@@ -0,0 +1,36 @@
+import chromadb
+import os
+import json
+import sys
+
+from chromadb.utils import embedding_functions
+from chromadb.config import Settings
+
+
+chroma_client = chromadb.Client(Settings(chroma_db_impl='duckdb+parquet',persist_directory='db'))
+
+openai_ef = embedding_functions.OpenAIEmbeddingFunction(
+                api_key=os.environ['OPENAI_API_KEY'],
+                model_name="text-embedding-ada-002"
+            )
+
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Syntax loadFlows.py [flow_direction]")
+    else:
+        print("Loading collection")
+        collection = chroma_client.create_collection(name="flow_collection",embedding_function=openai_ef)
+
+        flowInfo = {}
+        for fl in os.listdir(sys.argv[1]):
+            f = open(os.path.join(sys.argv[1],fl),"r")
+            j = json.loads(f.read())
+            flowInfo[j['name']] = j['@id']
+        names = list(flowInfo.keys())
+        values = [flowInfo[x] for x in names]
+        length = 2000
+        for n in range(len(flowInfo)//length):
+            collection.add(documents=list(names[(n*length):(n*length+length)]),ids=list(values[(n*length):(n*length+length)]))
+        chroma_client.persist()
